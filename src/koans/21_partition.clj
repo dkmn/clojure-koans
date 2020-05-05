7522381016bbc8d9791dbeaad9dc8d4f534a1469
David K. Kim <david@radardoc.net>
2020-05-04 19:07:11 -0700

    Dad's solutions to the partition exercises (partial).

--- src/koans/21_partition.clj
+++ src/koans/21_partition.clj
@@ -3,13 +3,13 @@
 
 (meditations
   "To split a collection you can use the partition function"
-  (= '((0 1) (2 3)) (__ 2 (range 4)))
+  (= '((0 1) (2 3)) (partition 2 (range 4)))
 
   "But watch out if there are not enough elements to form n sequences"
-  (= '(__) (partition 3 [:a :b :c :d :e]))
+  (= '((:a :b :c)) (partition 3 [:a :b :c :d :e]))
 
   "You can use partition-all to include any leftovers too"
-  (= __ (partition-all 3 (range 5)))
+  (= '((0 1 2) (3 4 5)) (partition-all 3 (range 5)))
 
   "If you need to, you can start each sequence with an offset"
   (= '((0 1 2) (5 6 7) (10 11 12)) (partition 3 __ (range 13)))
