115d6199fbab288fa54bab5b97a74eb1617fa840
David K. Kim <david@radardoc.net>
2020-05-19 15:46:19 -0700

    checkpt

--- src/koans/24_macros.clj
+++ src/koans/24_macros.clj
@@ -9,8 +9,8 @@
 
 (defmacro infix-concise [form]
   `(~(second form) ; Note the syntax-quote (`) and unquote (~) characters!
-    __
-    __))
+    ~(first form)
+    ~(nth form 2)))
 
 (defmacro recursive-infix [form]
   (cond (not (seq? form))
@@ -27,19 +27,20 @@
 
 (meditations
   "Macros are like functions created at compile time"
-  (= __ (hello "Macros!"))
+  (= "Hello, Macros!" (hello "Macros!"))
 
   "I can haz infix?"
-  (= __ (infix (9 + 1)))
+  (= 10 (infix (9 + 1)))
 
   "Remember, these are nothing but code transformations"
-  (= __ (macroexpand '(infix (9 + 1))))
+  (= '(+ 9 1) (macroexpand '(infix (9 + 1))))
 
   "You can do better than that - hand crafting FTW!"
   (= '(* 10 2) (macroexpand '(infix-concise (10 * 2))))
 
   "Things don't always work as you would like them to... "
   (= '(+ 10 (2 * 3)) (macroexpand '(infix-concise (10 + (2 * 3)))))
+
 
   "Really, you don't understand recursion until you understand recursion"
   (= 36 (recursive-infix (10 + (2 * 3) + (4 * 5)))))
