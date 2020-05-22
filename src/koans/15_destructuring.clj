94cdf670e8e21ef5efc0a1981455c3b6491f89dc
Kai Kim <kaihkim14@gmail.com>
2020-05-21 17:36:17 -0700

    Finished Destructuring

--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -9,40 +9,44 @@
 (meditations
   "Destructuring is an arbiter: it breaks up arguments"
   (= ":bar:foo" ((fn [[a b]] (str b a))
-         [:foo :bar]))
+                 [:foo :bar]))
 
   "Whether in function definitions"
   (= (str "An Oxford comma list of apples, "
           "oranges, "
           "and pears.")
-     ((fn [[a b c]] ("An Oxford comma list of "a" ",
-                      " "b" ",
-                      "and "c".")
-      ["apples" "oranges" "pears"])))
+     ((fn [[a b c]] (str "An Oxford comma list of " a ","
+                         " " b ", "
+                         "and " c "."))
+      ["apples" "oranges" "pears"]))
 
   "Or in let expressions"
   (= "Rich Hickey aka The Clojurer aka Go Time aka Lambda Guru"
-      (let [[first-name last-name & aliases] (list "Rich" "Hickey" "The Clojurer" "Go Time" "Macro Killah")]
-        (str first-name " " (apply str (interpose " aka " (conj aliases last-name))))))
+     (let [[first-name last-name & aliases] (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
+       (str first-name " " (apply str (interpose " aka " (conj aliases last-name))))))
 
 
-     "You can regain the full argument if you like arguing"
+
+  "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
      (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
-       {:original-parts full-name :named-parts {:first first-name :last last-name}} ))))
+       {:original-parts full-name :named-parts {:first first-name :last last-name}}))
 
   "Break up maps by key"
   (= "123 Test Lane, Testerville, TX"
      (let [{street-address :street-address, city :city, state :state} test-address]
-       (apply str (interpose ", " [street-address city state]))))))
+       (apply str (interpose ", " [street-address city state]))))
 
   "Or more succinctly"
   (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address  city state]} test-address]
-       (apply str (interpose ", " [street-address city state]))))))
+     (let [{:keys [street-address city state]} test-address]
+       (apply str (interpose ", " [street-address city state]))))
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (( (fn [v m] (let [{:keys [city state street-address]} m]  (apply str (interpose ", "
-                                                                                       [(apply str (interpose " " v)) street-address city state]))))
-        ["Test" "Testerson"] test-address)))
+     (
+      (fn [v m] (let [{:keys [city state street-address]} m]
+                  (apply str
+                         (interpose ", " [(apply str (interpose " " v)) street-address city state]))))
+       ["Test" "Testerson"] test-address))
+  )
